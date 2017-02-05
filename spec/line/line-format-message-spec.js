218a2668bb9e4fece34a64e2b13f2de82009d5da
TaopaiC <pctao.blog@pctao.org>
2017-02-05 13:51:45 +0800

    Uppercase the first character of error messages in line-format-message

--- spec/line/line-format-message-spec.js
+++ spec/line/line-format-message-spec.js
@@ -41,23 +41,30 @@ describe('Line format message', () => {
   describe('Image', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.Image('https://example.com/imageurl', 'https://example.com/previewurl');
+
       expect(typeof formatLineMessage.Image).toBe('function');
       expect(message instanceof formatLineMessage.Image).toBeTruthy();
     });
+
     it('should throw an error if url is not provided', () => {
       expect(() => new formatLineMessage.Image()).toThrowError('Image message required a valid URL as a first parameter');
     });
+
     it('should throw an error if url is not a valid url', () => {
       expect(() => new formatLineMessage.Image('imageurl')).toThrowError('Image message required a valid URL as a first parameter');
       expect(() => new formatLineMessage.Image('https://example.com/imageurl', 'previewurl')).toThrowError('Image message required a valid URL as a second parameter');
     });
+
     it('should throw an error if url is too long', () => {
       const longurl = new Array(99).join('0123456789');
-      expect(() => new formatLineMessage.Image(`https://example.com/${longurl}/`)).toThrowError('originalContentUrl can not be more than 1000 characters');
-      expect(() => new formatLineMessage.Image('https://example.com/imageurl', `https://example.com/${longurl}/`)).toThrowError('previewImageUrl can not be more than 1000 characters');
+
+      expect(() => new formatLineMessage.Image(`https://example.com/${longurl}/`)).toThrowError('OriginalContentUrl can not be more than 1000 characters');
+      expect(() => new formatLineMessage.Image('https://example.com/imageurl', `https://example.com/${longurl}/`)).toThrowError('PreviewImageUrl can not be more than 1000 characters');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.Image('https://example.com/imageurl', 'https://example.com/previewurl');
+
       expect(message.get()).toEqual({
         type: 'image',
         originalContentUrl: 'https://example.com/imageurl',
@@ -69,25 +76,31 @@ describe('Line format message', () => {
   describe('Video', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.Video('https://example.com/videourl', 'https://example.com/previewurl');
+
       expect(typeof formatLineMessage.Video).toBe('function');
       expect(message instanceof formatLineMessage.Video).toBeTruthy();
     });
+
     it('should throw an error if url is not provided', () => {
       expect(() => new formatLineMessage.Video()).toThrowError('Video message required a valid URL as a first parameter');
     });
+
     it('should throw an error if url is not a valid url', () => {
       expect(() => new formatLineMessage.Video('videourl')).toThrowError('Video message required a valid URL as a first parameter');
       expect(() => new formatLineMessage.Video('https://example.com/videourl', 'previewurl')).toThrowError('Video message required a valid URL as a second parameter');
     });
+
     it('should throw an error if url is too long', () => {
       const longtext = new Array(99).join('0123456789');
       const longurl = `https://example.com/${longtext}/`;
 
-      expect(() => new formatLineMessage.Video(longurl)).toThrowError('originalContentUrl can not be more than 1000 characters');
-      expect(() => new formatLineMessage.Video('https://example.com/videourl', longurl)).toThrowError('previewImageUrl can not be more than 1000 characters');
+      expect(() => new formatLineMessage.Video(longurl)).toThrowError('OriginalContentUrl can not be more than 1000 characters');
+      expect(() => new formatLineMessage.Video('https://example.com/videourl', longurl)).toThrowError('PreviewImageUrl can not be more than 1000 characters');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.Video('https://example.com/videourl', 'https://example.com/previewurl');
+
       expect(message.get()).toEqual({
         type: 'video',
         originalContentUrl: 'https://example.com/videourl',
@@ -99,25 +112,33 @@ describe('Line format message', () => {
   describe('Audio', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.Audio('https://example.com/audiourl', 20);
+
       expect(typeof formatLineMessage.Audio).toBe('function');
       expect(message instanceof formatLineMessage.Audio).toBeTruthy();
     });
+
     it('should throw an error if url is not provided', () => {
       expect(() => new formatLineMessage.Audio()).toThrowError('Audio message required a valid URL as a first parameter');
     });
+
     it('should throw an error if url is not a valid url', () => {
       expect(() => new formatLineMessage.Audio('audiourl')).toThrowError('Audio message required a valid URL as a first parameter');
     });
+
     it('should throw an error if duration is not a valid number', () => {
       expect(() => new formatLineMessage.Audio('https://example.com/audiourl')).toThrowError('Audio message required a valid duration as a second parameter');
       expect(() => new formatLineMessage.Audio('https://example.com/audiourl', 't')).toThrowError('Audio message required a valid duration as a second parameter');
     });
+
     it('should throw an error if url is too long', () => {
       const longurl = new Array(99).join('0123456789');
-      expect(() => new formatLineMessage.Audio(`https://example.com/${longurl}/`, 20)).toThrowError('originalContentUrl can not be more than 1000 characters');
+
+      expect(() => new formatLineMessage.Audio(`https://example.com/${longurl}/`, 20)).toThrowError('OriginalContentUrl can not be more than 1000 characters');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.Audio('https://example.com/audiourl', 20);
+
       expect(message.get()).toEqual({
         type: 'audio',
         originalContentUrl: 'https://example.com/audiourl',
@@ -129,26 +150,33 @@ describe('Line format message', () => {
   describe('Location', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.Location('Location title', 'Somewhere on earth', 20, 44);
+
       expect(typeof formatLineMessage.Location).toBe('function');
       expect(message instanceof formatLineMessage.Location).toBeTruthy();
     });
+
     it('should throw an error if title, address, latitude or longitude is not provided', () => {
       expect(() => new formatLineMessage.Location(undefined, 'Earth', 20, 44)).toThrowError('Location message required a title as a first parameter');
       expect(() => new formatLineMessage.Location('Location title', undefined, 20, 44)).toThrowError('Location message required an address as a second parameter');
       expect(() => new formatLineMessage.Location('Location title', 'Earth', undefined, 44)).toThrowError('Location message required a latitude as a third parameter');
       expect(() => new formatLineMessage.Location('Location title', 'Earth', 20)).toThrowError('Location message required a longitude as a fourth parameter');
     });
+
     it('should throw an error if latitude or longitude is not a valid number', () => {
       expect(() => new formatLineMessage.Location('Location title', 'Earth', 't', 44)).toThrowError('Location message required a latitude as a third parameter');
       expect(() => new formatLineMessage.Location('Location title', 'Earth', 20, 't')).toThrowError('Location message required a longitude as a fourth parameter');
     });
+
     it('should throw an error if title or address is too long', () => {
       const longtext = new Array(11).join('0123456789') + '1';
-      expect(() => new formatLineMessage.Location(longtext, 'Earth', 20, 44)).toThrowError('title can not be more than 100 characters');
-      expect(() => new formatLineMessage.Location('Location title', longtext, 20, 44)).toThrowError('address can not be more than 100 characters');
+
+      expect(() => new formatLineMessage.Location(longtext, 'Earth', 20, 44)).toThrowError('Title can not be more than 100 characters');
+      expect(() => new formatLineMessage.Location('Location title', longtext, 20, 44)).toThrowError('Address can not be more than 100 characters');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.Location('Location title', 'Earth', 20, 44);
+
       expect(message.get()).toEqual({
         type: 'location',
         title: 'Location title',
@@ -158,19 +186,24 @@ describe('Line format message', () => {
       });
     });
   });
+
   describe('Sticker', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.Sticker('123', '456');
+
       expect(typeof formatLineMessage.Sticker).toBe('function');
       expect(message instanceof formatLineMessage.Sticker).toBeTruthy();
     });
+
     it('should throw an error if packageId or stickerId is not provided', () => {
       expect(() => new formatLineMessage.Sticker()).toThrowError('Sticker message required a packageId as a first parameter');
       expect(() => new formatLineMessage.Sticker('123')).toThrowError('Sticker message required a stickerId as a second parameter');
       expect(() => new formatLineMessage.Sticker(undefined, '456')).toThrowError('Sticker message required a packageId as a first parameter');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.Sticker('123', '456');
+
       expect(message.get()).toEqual({
         type: 'sticker',
         packageId: '123',
@@ -178,51 +211,66 @@ describe('Line format message', () => {
       });
     });
   });
+
   describe('ImageMap', () => {
     it('should be a class', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+
       expect(typeof formatLineMessage.ImageMap).toBe('function');
       expect(message instanceof formatLineMessage.ImageMap).toBeTruthy();
     });
+
     it('should throw an error if no action are added', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+
       expect(() => message.get()).toThrowError('Add at least one action first!');
     });
+
     it('should throw an error if more than 50 actions are added', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+
       for(let i = 0; i < 50; i++) {
         message.addMessageAction('text', {x: 20, y: 20, width: 20, height: 20});
       }
+
       expect(() => {
         message.addMessageAction('text', {x: 20, y: 20, width: 20, height: 20});
       }).toThrowError('There can not be more than 50 actions');
     });
+
     it('should throw an error if url is not valid for addUriAction', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+
       expect(() => {
         message.addUriAction('text', {x: 20, y: 20, width: 20, height: 20});
-      }).toThrowError('linkUri has a bad url');
+      }).toThrowError('LinkUri has a bad url');
     });
+
     it('should throw an error if url is too long for addUriAction', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+      const longtext = new Array(99).join('0123456789');
+      const longurl = `https://example.com/${longtext}/`;
+
       expect(() => {
-        const longtext = new Array(99).join('0123456789');
-        const longurl = `https://example.com/${longtext}/`;
         message.addUriAction(longurl, {x: 20, y: 20, width: 20, height: 20});
-      }).toThrowError('linkUri can not be more than 1000 characters');
+      }).toThrowError('LinkUri can not be more than 1000 characters');
     });
+
     it('should throw an error if text is too long for addMessageAction', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text');
+      const longtext = new Array(41).join('0123456789');
+      const longurl = `${longtext}` + '1';
+
       expect(() => {
-        const longtext = new Array(41).join('0123456789');
-        const longurl = `${longtext}` + '1';
         message.addMessageAction(longurl, {x: 20, y: 20, width: 20, height: 20});
-      }).toThrowError('text can not be more than 400 characters');
+      }).toThrowError('Text can not be more than 400 characters');
     });
+
     it('should return a simple message object', () => {
       const message = new formatLineMessage.ImageMap('https://example.com/image/1/', 400, 300, 'alt text')
         .addUriAction('https://example.com/a', {x: 10, y: 10, width: 10, height: 10})
         .addMessageAction('text', {x: 20, y: 20, width: 20, height: 20});
+
       expect(message.get()).toEqual({
         type: 'imagemap',
         baseUrl: 'https://example.com/image/1/',
@@ -283,10 +331,10 @@ describe('Line format message', () => {
       });
 
       it('should throw an error if label or url is not valid', () => {
-        expect(() => { message.addUriAction('label', 'text'); }).toThrowError('action required a uri as a second parameter');
-        expect(() => { message.addUriAction('label'); }).toThrowError('action required a uri as a second parameter');
-        expect(() => { message.addUriAction(undefined, 'https://example.com/'); }).toThrowError('action required a label as a first parameter');
-        expect(() => { message.addUriAction(); }).toThrowError('action required a label as a first parameter');
+        expect(() => { message.addUriAction('label', 'text'); }).toThrowError('Action required a uri as a second parameter');
+        expect(() => { message.addUriAction('label'); }).toThrowError('Action required a uri as a second parameter');
+        expect(() => { message.addUriAction(undefined, 'https://example.com/'); }).toThrowError('Action required a label as a first parameter');
+        expect(() => { message.addUriAction(); }).toThrowError('Action required a label as a first parameter');
       });
 
       it('should throw an error if url is too long', () => {
@@ -294,14 +342,14 @@ describe('Line format message', () => {
         const longurl = `https://example.com/${longtext}/`;
 
         expect(longurl.length).toEqual(1001);
-        expect(() => { message.addUriAction('label', longurl); }).toThrowError('uri can not be more than 1000 characters');
+        expect(() => { message.addUriAction('label', longurl); }).toThrowError('Uri can not be more than 1000 characters');
       });
 
       it('should throw an error if label is too long', () => {
         const longtext = new Array(3).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(21);
-        expect(() => { message.addUriAction(longtext, 'https://example.com/'); }).toThrowError('label can not be more than 20 characters');
+        expect(() => { message.addUriAction(longtext, 'https://example.com/'); }).toThrowError('Label can not be more than 20 characters');
       });
 
       it('should add a UriButton', () => {
@@ -349,23 +397,23 @@ describe('Line format message', () => {
       });
 
       it('should throw an error if label or message is not valid', () => {
-        expect(() => { message.addMessageAction('label'); }).toThrowError('action required a text as a second parameter');
-        expect(() => { message.addMessageAction(undefined, 'text'); }).toThrowError('action required a label as a first parameter');
-        expect(() => { message.addMessageAction(); }).toThrowError('action required a label as a first parameter');
+        expect(() => { message.addMessageAction('label'); }).toThrowError('Action required a text as a second parameter');
+        expect(() => { message.addMessageAction(undefined, 'text'); }).toThrowError('Action required a label as a first parameter');
+        expect(() => { message.addMessageAction(); }).toThrowError('Action required a label as a first parameter');
       });
 
       it('should throw an error if message is too long', () => {
         const longtext = new Array(31).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(301);
-        expect(() => { message.addMessageAction('label', longtext); }).toThrowError('text can not be more than 300 characters');
+        expect(() => { message.addMessageAction('label', longtext); }).toThrowError('Text can not be more than 300 characters');
       });
 
       it('should throw an error if label is too long', () => {
         const longtext = new Array(3).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(21);
-        expect(() => { message.addMessageAction(longtext, 'text'); }).toThrowError('label can not be more than 20 characters');
+        expect(() => { message.addMessageAction(longtext, 'text'); }).toThrowError('Label can not be more than 20 characters');
       });
 
       it('should add a message button', () => {
@@ -413,31 +461,31 @@ describe('Line format message', () => {
       });
 
       it('should throw an error if label, data, or message is not valid', () => {
-        expect(() => { message.addPostbackAction(); }).toThrowError('postback action required a label as a first parameter');
-        expect(() => { message.addPostbackAction('label'); }).toThrowError('postback action required a data as a second parameter');
-        expect(() => { message.addPostbackAction(undefined, 'data'); }).toThrowError('postback action required a label as a first parameter');
-        expect(() => { message.addPostbackAction('label', 'data', 12); }).toThrowError('text needs to be a string');
+        expect(() => { message.addPostbackAction(); }).toThrowError('Postback action required a label as a first parameter');
+        expect(() => { message.addPostbackAction('label'); }).toThrowError('Postback action required a data as a second parameter');
+        expect(() => { message.addPostbackAction(undefined, 'data'); }).toThrowError('Postback action required a label as a first parameter');
+        expect(() => { message.addPostbackAction('label', 'data', 12); }).toThrowError('Text needs to be a string');
       });
 
       it('should throw an error if message is too long', () => {
         const longtext = new Array(31).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(301);
-        expect(() => { message.addPostbackAction('label', 'data', longtext); }).toThrowError('text can not be more than 300 characters');
+        expect(() => { message.addPostbackAction('label', 'data', longtext); }).toThrowError('Text can not be more than 300 characters');
       });
 
       it('should throw an error if data is too long', () => {
         const longtext = new Array(31).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(301);
-        expect(() => { message.addPostbackAction('label', longtext); }).toThrowError('data can not be more than 300 characters');
+        expect(() => { message.addPostbackAction('label', longtext); }).toThrowError('Data can not be more than 300 characters');
       });
 
       it('should throw an error if label is too long', () => {
         const longtext = new Array(3).join('0123456789') + '1';
 
         expect(longtext.length).toEqual(21);
-        expect(() => { message.addPostbackAction(longtext, 'text'); }).toThrowError('label can not be more than 20 characters');
+        expect(() => { message.addPostbackAction(longtext, 'text'); }).toThrowError('Label can not be more than 20 characters');
       });
 
       it('should add a postback button', () => {
@@ -521,15 +569,4 @@ describe('Line format message', () => {
   describe('Carousel', () => {
   });
 });
-
-
-
-
-
-
-
-
-
-
-
 
